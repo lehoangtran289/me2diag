d8b77de593d8d6595cd3cd5a96924c587b9ffb48
lehoangtran289 <lehoangtran289@gmail.com>
2022-02-27 00:30:15 +0700

    pfs sql insert

--- backend/src/main/resources/script/picture_fuzzy.sql
+++ backend/src/main/resources/script/picture_fuzzy.sql
@@ -72,3 +72,41 @@ CREATE TABLE `examination_result`
     `created_at`     DATETIME
 );
 
+INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('CHEST_PROBLEM', null, '2022-02-26 18:04:40', '2022-02-26 18:04:46');
+INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('FEVER', null, '2022-02-26 18:04:38', '2022-02-26 18:04:44');
+INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('MALARIA', null, '2022-02-26 18:04:39', '2022-02-26 18:04:44');
+INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('STOMACH', null, '2022-02-26 18:04:40', '2022-02-26 18:04:45');
+INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('TYPHOID', null, '2022-02-26 18:04:39', '2022-02-26 18:04:45');
+
+INSERT INTO backend.symptom (name, description, created_at, updated_at) VALUES ('CHEST_PAIN', null, '2022-02-26 18:03:48', '2022-02-26 18:03:50');
+INSERT INTO backend.symptom (name, description, created_at, updated_at) VALUES ('COUGH', null, '2022-02-26 18:03:43', '2022-02-26 18:03:47');
+INSERT INTO backend.symptom (name, description, created_at, updated_at) VALUES ('HEADACHE', null, '2022-02-26 18:02:12', '2022-02-26 18:02:17');
+INSERT INTO backend.symptom (name, description, created_at, updated_at) VALUES ('STOMACH_PAIN', null, '2022-02-26 18:02:50', '2022-02-26 18:03:47');
+INSERT INTO backend.symptom (name, description, created_at, updated_at) VALUES ('TEMPERATURE', null, '2022-02-26 18:01:55', '2022-02-26 18:01:57');
+
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('CHEST_PAIN', 'CHEST_PROBLEM', 0.9, 0.02, 0.05, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('CHEST_PAIN', 'FEVER', 0.05, 0.25, 0.6, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('CHEST_PAIN', 'MALARIA', 0.03, 0.07, 0.8, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('CHEST_PAIN', 'STOMACH', 0.1, 0.1, 0.7, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('CHEST_PAIN', 'TYPHOID', 0.01, 0.01, 0.85, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('COUGH', 'CHEST_PROBLEM', 0.15, 0.2, 0.7, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('COUGH', 'FEVER', 0.45, 0.2, 0.1, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('COUGH', 'MALARIA', 0.65, 0.5, 0.05, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('COUGH', 'STOMACH', 0.25, 0.25, 0.5, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('COUGH', 'TYPHOID', 0.2, 0.15, 0.6, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('HEADACHE', 'CHEST_PROBLEM', 0.01, 0.1, 0.8, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('HEADACHE', 'FEVER', 0.4, 0.25, 0.3, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('HEADACHE', 'MALARIA', 0.1, 0.2, 0.6, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('HEADACHE', 'STOMACH', 0.3, 0.05, 0.05, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('HEADACHE', 'TYPHOID', 0.75, 0.05, 0.03, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('STOMACH_PAIN', 'CHEST_PROBLEM', 0.1, 0.15, 0.75, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('STOMACH_PAIN', 'FEVER', 0.1, 0.25, 0.6, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('STOMACH_PAIN', 'MALARIA', 0.01, 0.03, 0.9, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('STOMACH_PAIN', 'STOMACH', 0.8, 0.1, 0.01, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('STOMACH_PAIN', 'TYPHOID', 0.1, 0.2, 0.7, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'CHEST_PROBLEM', 0.05, 0.15, 0.7, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'FEVER', 0.4, 0.4, 0.05, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'MALARIA', 0.8, 0.1, 0.1, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'STOMACH', 0.15, 0.05, 0.6, '2022-02-26 18:07:59');
+INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'TYPHOID', 0.3, 0.3, 0.3, '2022-02-26 18:07:59');
+
