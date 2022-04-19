ba4052b6a830111985eebc3ebf745deaa6bf0e27
lehoangtran289 <lehoangtran289@gmail.com>
2022-04-19 08:30:04 +0700

    hedge algebra sql init

--- backend/src/main/resources/script/picture_fuzzy.sql
+++ backend/src/main/resources/script/picture_fuzzy.sql
@@ -71,6 +71,26 @@ CREATE TABLE `examination_result`
     PRIMARY KEY (examination_id, diagnose)
 );
 
+CREATE TABLE `hedge_algebra_config`
+(
+    `name`  VARCHAR(50) PRIMARY KEY,
+    `fm` DOUBLE,
+    `type`  VARCHAR(50),
+    `order` INTEGER,
+    `created_at`  DATETIME,
+    `updated_at`  DATETIME
+);
+
+CREATE TABLE `linguistic_domain`
+(
+    `name` VARCHAR(50) PRIMARY KEY,
+    `fm` DOUBLE,
+    `v` DOUBLE,
+    `order` INTEGER,
+    `created_at`  DATETIME,
+    `updated_at`  DATETIME
+);
+
 INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('CHEST_PROBLEM', null, '2022-02-26 18:04:40', '2022-02-26 18:04:46');
 INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('FEVER', null, '2022-02-26 18:04:38', '2022-02-26 18:04:44');
 INSERT INTO backend.diagnose (name, description, created_at, updated_at) VALUES ('MALARIA', null, '2022-02-26 18:04:39', '2022-02-26 18:04:44');
@@ -109,3 +129,20 @@ INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, nega
 INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'STOMACH', 0.15, 0.05, 0.6, '2022-02-26 18:07:59');
 INSERT INTO backend.symptom_diagnose (symptom, diagnose, positive, neutral, negative, created_at) VALUES ('TEMPERATURE', 'TYPHOID', 0.3, 0.3, 0.3, '2022-02-26 18:07:59');
 
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('COMPLETELY', 1, 1, 99, '2022-04-18 22:54:51', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('HIGH', 0.5, 0.75, 3, '2022-04-18 22:54:49', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('LOW', 0.5, 0.25, -1, '2022-04-18 22:54:47', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('MEDIUM', 0.5, 0.5, 0, '2022-04-18 22:54:48', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('NONE', 0, 0, -99, '2022-04-18 22:54:44', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('SLIGHTLY HIGH', 0.25, 0.625, 1, '2022-04-18 22:54:49', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('SLIGHTLY LOW', 0.25, 0.375, -2, '2022-04-18 22:54:46', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('VERY HIGH', 0.25, 0.875, 2, '2022-04-18 22:54:50', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('VERY LOW', 0.25, 0.125, -3, '2022-04-18 22:54:48', null);
+
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('COMPLETELY', 99, 'GENERATOR', 99, '2022-04-18 22:30:27', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('HIGH', 0.5, 'GENERATOR', 1, '2022-04-18 22:30:25', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('LOW', 0.5, 'GENERATOR', -1, '2022-04-18 22:30:23', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('MEDIUM', 0.5, 'GENERATOR', 0, '2022-04-18 22:30:24', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('NONE', 0, 'GENERATOR', -99, '2022-04-18 22:30:20', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('SLIGHTLY', 0.5, 'HEDGE', -1, '2022-04-18 22:30:23', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('VERY', 0.5, 'HEDGE', 1, '2022-04-18 22:30:22', null);
