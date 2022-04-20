0197c2fb4412c0a4f43c9830ffa187e875b4880f
lehoangtran289 <lehoangtran289@gmail.com>
2022-04-20 18:06:03 +0700

    data sql

---
+++ backend/src/main/resources/script/backend_hedge_algebra_config.sql
@@ -0,0 +1,7 @@
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('COMPLETELY', 99, 'GENERATOR', 99, '2022-04-18 22:30:27', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('HIGH', 0.5, 'GENERATOR', 1, '2022-04-18 22:30:25', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('LOW', 0.5, 'GENERATOR', -1, '2022-04-18 22:30:23', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('MEDIUM', 0.5, 'GENERATOR', 0, '2022-04-18 22:30:24', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('NONE', 0, 'GENERATOR', -99, '2022-04-18 22:30:20', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('SLIGHTLY', 0.5, 'HEDGE', -1, '2022-04-18 22:30:23', null);
+INSERT INTO backend.hedge_algebra_config (name, fm, type, `order`, created_at, updated_at) VALUES ('VERY', 0.5, 'HEDGE', 1, '2022-04-18 22:30:22', null);
---
+++ backend/src/main/resources/script/backend_linguistic_domain.sql
@@ -0,0 +1,9 @@
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('COMPLETELY', 1, 1, 99, '2022-04-18 22:54:51', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('HIGH', 0.5, 0.75, 3, '2022-04-18 22:54:49', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('LOW', 0.5, 0.25, -1, '2022-04-18 22:54:47', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('MEDIUM', 0.5, 0.5, 0, '2022-04-18 22:54:48', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('NONE', 0, 0, -99, '2022-04-18 22:54:44', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('SLIGHTLY_HIGH', 0.25, 0.625, 1, '2022-04-18 22:54:49', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('SLIGHTLY_LOW', 0.25, 0.375, -2, '2022-04-18 22:54:46', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('VERY_HIGH', 0.25, 0.875, 2, '2022-04-18 22:54:50', null);
+INSERT INTO backend.linguistic_domain (name, fm, v, `order`, created_at, updated_at) VALUES ('VERY_LOW', 0.25, 0.125, -3, '2022-04-18 22:54:48', null);
