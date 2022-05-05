e1a4ad9272a299d9c0d323da9feedaf682cca1bb
lehoangtran289 <lehoangtran289@gmail.com>
2022-05-05 16:36:37 +0700

    init backend prediction service

---
+++ backend-model/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify, render_template
+import json
+import numpy as np
+import pandas as pd
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/kdc/api/v1/predict', methods=['POST'])
+def predict():
+    data = request.get_json(force=True)
+    print(data)
+    print(type(data))
+    x_test = pd.DataFrame([data], columns=data.keys())
+    print(x_test)
+    prediction = model.predict(x_test)
+
+    print(prediction)
+    return str(prediction)
+    
+if __name__ == '__main__':
+    app.run(debug=True)
